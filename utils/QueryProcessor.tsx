0ba95aa08c756fdfcae65910040e8529a98edf0b
Nour Ali <ntali@andrew.cmu.ed>
2024-10-24 15:20:11 +0300

    others

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -31,5 +31,46 @@ export default function QueryProcessor(query: string): string {
     return "";
 
   }
+
+  if (query.toLowerCase().includes("plus")) {
+    const matchedNumbers = query.match(/\d+/g); // Extract all numbers from the query
+
+    if (matchedNumbers && matchedNumbers.length >= 2) {
+      const numbers: number[] = matchedNumbers.map(Number); // Convert the matched strings to numbers
+      const sum = numbers.reduce((acc, curr) => acc + curr, 0); // Sum the first two numbers
+      return sum.toString();
+    } 
+    return ""
+  }
+
+  if (query.toLowerCase().includes("multiplied")) {
+    const matchedNumbers = query.match(/\d+/g); // Extract all numbers from the query
+
+    if (matchedNumbers && matchedNumbers.length >= 2) {
+      const numbers: number[] = matchedNumbers.map(Number); // Convert the matched strings to numbers
+      const sum = numbers.reduce((acc, curr) => acc * curr, 1); // multiply the first two numbers
+      return sum.toString();
+    } 
+    return ""
+  }
+
+  if (query.toLowerCase().includes("square and a cube")) {
+    const matchedNumbers = query.match(/\d+/g); // Extract all numbers from the query
+
+    if (matchedNumbers) {
+      const numbers: number[] = matchedNumbers.map(Number); // Convert the matched strings to numbers
+  
+      for (const num of numbers) {
+        const sqrt = Math.sqrt(num);
+        const cubeRoot = Math.cbrt(num);
+  
+        if (Math.floor(sqrt) === sqrt && Math.floor(cubeRoot) === cubeRoot) {
+          return num.toString(); // Return the first number that satisfies the condition
+        }
+      }
+    }
+  }
   return "";
 }
+
+
